678debb6e7234fbdef58948a67969d221257eae5
Sam Hoffman <sam@weller.gentoo.party>
2026-02-23 13:12:42 -0500

    reorganize linux screen share

--- app/internal/live/screen_linux.go
+++ app/internal/live/screen_linux.go
@@ -7,6 +7,12 @@ import (
 	"log"
 	"os/exec"
 	"syscall"
+	"time"
+
+	lksdk "github.com/livekit/server-sdk-go/v2"
+	"github.com/pion/webrtc/v4/pkg/media"
+	"github.com/tinyzimmer/go-gst/gst"
+	"github.com/tinyzimmer/go-gst/gst/app"
 )
 
 func GetWindows() ([]WindowData, error) {
@@ -76,4 +82,64 @@ func ffmpegScreenShare(w *WindowData) (*StreamedProcess, error) {
 		Setsid: true,
 	}
 	return proc, nil
+}
+
+type GstStreamer struct {
+	pipeline *gst.Pipeline
+	track    *lksdk.LocalTrack
+}
+
+func NewGstScreenShare(w *WindowData, track *lksdk.LocalTrack) (*GstStreamer, error) {
+	gst.Init(nil)
+
+	pipelineStr := fmt.Sprintf(
+		"ximagesrc xid=%d use-damage=0 ! "+
+			"videoconvert ! "+
+			"videoscale ! "+
+			"video/x-raw,format=I420,framerate=30/1,width=%d,height=%d ! "+
+			"x264enc tune=zerolatency speed-preset=ultrafast key-int-max=30 ! "+
+			"h264parse config-interval=-1 ! "+
+			"video/x-h264,stream-format=byte-stream,alignment=au ! "+ // FORCE ANNEX-B
+			"appsink name=sink sync=false emit-signals=true drop=true max-buffers=1",
+		w.ID, w.W&^1, w.H&^1,
+	)
+
+	log.Println(pipelineStr)
+
+	pipeline, err := gst.NewPipelineFromString(pipelineStr)
+	if err != nil {
+		return nil, err
+	}
+
+	sinkElement, _ := pipeline.GetElementByName("sink")
+	sink := app.SinkFromElement(sinkElement)
+
+	sink.SetCallbacks(&app.SinkCallbacks{
+		NewSampleFunc: func(s *app.Sink) gst.FlowReturn {
+			sample := s.PullSample()
+			if sample == nil {
+				return gst.FlowEOS
+			}
+			buffer := sample.GetBuffer()
+			data := buffer.Bytes()
+
+			err := track.WriteSample(media.Sample{
+				Data:     data,
+				Duration: time.Second / 30,
+			}, nil)
+			if err != nil {
+				return gst.FlowError
+			}
+
+			return gst.FlowOK
+		},
+	})
+
+	return &GstStreamer{
+		pipeline: pipeline,
+	}, nil
+}
+
+func (g *GstStreamer) Start() error {
+	return g.pipeline.SetState(gst.StatePlaying)
 }
