0abeae406ccb5d7a85571546ee3cfdbaebeeb691
Sam Hoffman <sam@weller.gentoo.party>
2026-02-27 01:24:18 -0500

    use audiornnoise for echo cancellation

--- app/internal/live/microphone_linux.go
+++ app/internal/live/microphone_linux.go
@@ -7,11 +7,14 @@ import (
 )
 
 func NewMicrophone(track *lksdk.LocalTrack) (*GstTrackWriter, error) {
-	pipelineStr := "pulsesrc ! " +
+	pipelineStr := "pulsesrc buffer-time=20000 latency-time=5000 do-timestamp=true ! " +
+		"audioconvert ! " +
+		"audioresample ! " +
+		"audiornnoise ! " +
 		"audioconvert ! " +
 		"audioresample ! " +
 		"audio/x-raw,format=S16LE,layout=interleaved,rate=48000,channels=2 ! " +
-		"opusenc bitrate=64000 frame-size=20 bitrate-type=vbr bandwidth=fullband ! " +
+		"opusenc dtx=true bitrate=64000 frame-size=20 bitrate-type=vbr bandwidth=fullband ! " +
 		"appsink name=sink sync=false emit-signals=true drop=true max-buffers=1"
 
 	return NewGstTrackWriter(
